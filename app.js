e6bf4401c066d7b7d4320016ee98f148fad9ba3d
Kendra Krzywicki <kendra.krzywicki@knights.ucf.edu>
2019-10-19 17:49:24 -0400

    redo for simple app

--- app.js
+++ app.js
@@ -1,35 +1,20 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const path = require('path')
+const express = require('express')
+const app = express()
+const port = 3000
 
-var indexRouter = require('./routes/index');
+app.use(express.static('public'))
 
-var app = express();
+app.get('/', (req, res) => res.sendFile('./public/index.html'))
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
+// invalid route
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+})
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+app.listen(port, () => console.log(`App listening on port ${port}!`))
